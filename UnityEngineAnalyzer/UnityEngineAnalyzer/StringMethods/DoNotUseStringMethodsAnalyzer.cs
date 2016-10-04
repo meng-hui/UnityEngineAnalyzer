d984d9608ba543bd7c0f36357b24a49f36726b3c
Vinny <vinny.dasilva@gmail.com>
2016-10-04 15:16:22 -0400

    Tighter Registration for Analysis

--- UnityEngineAnalyzer/UnityEngineAnalyzer/StringMethods/DoNotUseStringMethodsAnalyzer.cs
+++ UnityEngineAnalyzer/UnityEngineAnalyzer/StringMethods/DoNotUseStringMethodsAnalyzer.cs
@@ -12,25 +12,36 @@ namespace UnityEngineAnalyzer.StringMethods
     {
         private static readonly ImmutableHashSet<string> StringMethods = ImmutableHashSet.Create("SendMessage", "SendMessageUpwards", "BroadcastMessage", "Invoke", "InvokeRepeating");
         private static readonly ImmutableHashSet<string> Namespaces = ImmutableHashSet.Create("UnityEngine.Component", "UnityEngine.GameObject", "UnityEngine.MonoBehaviour");
-        
+
         public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create(DiagnosticDescriptors.DoNotUseStringMethods);
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.IdentifierName);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
         }
 
         private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            // check if any of the "string methods" are used
-            var identifierName = context.Node as IdentifierNameSyntax;
-            if (!StringMethods.Contains(identifierName.Identifier.ToString())) { return; }
+            var invocation = context.Node as InvocationExpressionSyntax;
+            if (invocation == null)
+            {
+                return;
+            }
 
+            var name = (invocation.Expression as MemberAccessExpressionSyntax)?.Name.Identifier.ToString();
+
+            // check if any of the "string" methods are used
+            if (!StringMethods.Contains(name)) { return; }
+
+
+            //TODO: the call below is receiving a "OverloadResolutionFailure" - I think this is because it did not load the Unity references properly
             // check if the method is the one from UnityEngine
-            var methodSymbol = context.SemanticModel.GetSymbolInfo(identifierName).Symbol as IMethodSymbol;
+            var symbolInfo = context.SemanticModel.GetSymbolInfo(invocation);
+            var methodSymbol = symbolInfo.Symbol as IMethodSymbol;
+
             if (Namespaces.Any(ns => methodSymbol?.ToString().StartsWith(ns) ?? false))
             {
-                var diagnostic = Diagnostic.Create(DiagnosticDescriptors.DoNotUseStringMethods, identifierName.GetLocation());
+                var diagnostic = Diagnostic.Create(DiagnosticDescriptors.DoNotUseStringMethods, invocation.GetLocation());
                 context.ReportDiagnostic(diagnostic);
             }
         }
