c98975eb19c3ab665d819793a2ca09386fae2bf2
Vinny <vinny.dasilva@gmail.com>
2016-10-07 11:34:35 -0700

    Removing some Analyzers
    If an Analzyer does not have a built-in workaround in Unity, it was removed - trying to avoid too much opinionated analyzers

--- UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticDescriptors.cs
+++ UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticDescriptors.cs
@@ -1,34 +1,13 @@
 ﻿using Microsoft.CodeAnalysis;
 using UnityEngineAnalyzer.AOT;
 using UnityEngineAnalyzer.CompareTag;
-using UnityEngineAnalyzer.Coroutines;
 using UnityEngineAnalyzer.EmptyMonoBehaviourMethods;
 using UnityEngineAnalyzer.FindMethodsInUpdate;
-using UnityEngineAnalyzer.OnGUI;
-using UnityEngineAnalyzer.StringMethods;
 
 namespace UnityEngineAnalyzer
 {
     static class DiagnosticDescriptors
     {
-        public static readonly DiagnosticDescriptor DoNotUseOnGUI = new DiagnosticDescriptor(
-            id: DiagnosticIDs.DoNotUseOnGUI,
-            title: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.Title), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.MessageFormat), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)),
-            category: DiagnosticCategories.GC, 
-            defaultSeverity: DiagnosticSeverity.Warning, 
-            isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseOnGUIResources.Description), DoNotUseOnGUIResources.ResourceManager, typeof(DoNotUseOnGUIResources)));
-
-        public static readonly DiagnosticDescriptor DoNotUseStringMethods = new DiagnosticDescriptor(
-            id: DiagnosticIDs.DoNotUseStringMethods,
-            title: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.Title), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.MessageFormat), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)),
-            category: DiagnosticCategories.StringMethods,
-            defaultSeverity: DiagnosticSeverity.Warning,
-            isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseStringMethodsResources.Description), DoNotUseStringMethodsResources.ResourceManager, typeof(DoNotUseStringMethodsResources)));
-
         public static readonly DiagnosticDescriptor EmptyMonoBehaviourMethod = new DiagnosticDescriptor(
             id: DiagnosticIDs.EmptyMonoBehaviourMethod,
             title: new LocalizableResourceString(nameof(EmptyMonoBehaviourMethodsResources.Title), EmptyMonoBehaviourMethodsResources.ResourceManager, typeof(EmptyMonoBehaviourMethodsResources)),
@@ -55,15 +34,6 @@ namespace UnityEngineAnalyzer
             defaultSeverity: DiagnosticSeverity.Warning,
             isEnabledByDefault: true,
             description: new LocalizableResourceString(nameof(DoNotUseFindMethodsInUpdateResources.Description), DoNotUseFindMethodsInUpdateResources.ResourceManager, typeof(DoNotUseFindMethodsInUpdateResources)));
-
-        public static readonly DiagnosticDescriptor DoNotUseCoroutines = new DiagnosticDescriptor(
-            id: DiagnosticIDs.DoNotUseCoroutines,
-            title: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.Title), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)),
-            messageFormat: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.MessageFormat), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)),
-            category: DiagnosticCategories.GC,
-            defaultSeverity: DiagnosticSeverity.Warning,
-            isEnabledByDefault: true,
-            description: new LocalizableResourceString(nameof(DoNotUseCoroutinesResources.Description), DoNotUseCoroutinesResources.ResourceManager, typeof(DoNotUseCoroutinesResources)));
 
         public static readonly DiagnosticDescriptor DoNotUseRemoting = new DiagnosticDescriptor(
             id: DiagnosticIDs.DoNotUseRemoting,
--- UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticIDs.cs
+++ UnityEngineAnalyzer/UnityEngineAnalyzer/DiagnosticIDs.cs
@@ -3,13 +3,11 @@ namespace UnityEngineAnalyzer
 {
     public static class DiagnosticIDs
     {
-        public const string DoNotUseOnGUI = "UEA0001";
-        public const string DoNotUseStringMethods = "UEA0002";
         public const string EmptyMonoBehaviourMethod = "UEA0003";
         public const string UseCompareTag = "UEA0004";
         public const string DoNotUseFindMethodsInUpdate = "UEA0005";
-        public const string DoNotUseCoroutines = "UEA0006";
 
+        //NOTES: These should probably be on their own analyzer - as they are not specific to Unity
         public const string DoNotUseRemoting = "AOT0001";
         public const string DoNotUseReflectionEmit = "AOT0002";
         public const string TypeGetType = "AOT0003";
