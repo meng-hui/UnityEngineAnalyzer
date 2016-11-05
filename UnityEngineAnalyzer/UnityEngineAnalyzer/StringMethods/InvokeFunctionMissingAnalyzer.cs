ad7ac35b5055a6d572c3fbdbc8c0603be2d116c6
Vinny <vinny.dasilva@gmail.com>
2016-11-04 21:12:30 -0700

    Creating new Analyzer

---
+++ UnityEngineAnalyzer/UnityEngineAnalyzer/StringMethods/InvokeFunctionMissingAnalyzer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Immutable;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace UnityEngineAnalyzer.StringMethods
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class InvokeFunctionMissingAnalyzer : DiagnosticAnalyzer
+    {
+        public override void Initialize(AnalysisContext context)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
